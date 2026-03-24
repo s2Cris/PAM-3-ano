fc82af0bcbc046b9f04b7cc69be89d89050ed00e
Cris <marcheticristian@gmail.com>
2026-03-24 14:27:47 -0300

    atv 2

--- primeiroApp/App.tsx
+++ primeiroApp/App.tsx
@@ -5,10 +5,10 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 
 export default function App() {
 
-  const [valor, setValor] = useState(0);
-  const [mensagem, setMensagem] = useState('');
+  const [valor, setValor] = useState(0); //variável de estado para o valor do contador
+  const [mensagem, setMensagem] = useState(''); //variável de estado para a mensagem de aviso
 
-  function corValor(){
+  function corValor(){  //função para definir a cor do valor do contador
     if (valor > 0) {
       return 'green';
     } else if (valor < 0) {
@@ -18,7 +18,7 @@ export default function App() {
     }
   }
 
-  function somarValor(){
+  function somarValor(){ //função para somar 1 ao valor do contador
     if (valor >= 10) {
       setMensagem("Você clicou muitas vezes!");
     } else {
@@ -26,7 +26,7 @@ export default function App() {
     }
   }
 
-  function subtrairValor(){
+  function subtrairValor(){ // função para subtrair 1 do valor do contador
     if (valor <= -10) {
       setMensagem("Você clicou muitas vezes!");
     } else {
@@ -34,7 +34,7 @@ export default function App() {
     }
   }
 
-  function zerarValor(){
+  function zerarValor(){  //função para zerar o valor do contador e a mensagem de aviso
     setValor(0);
     setMensagem('');
   }
@@ -42,36 +42,52 @@ export default function App() {
 
   return (
 
-    <View style={styles.container}>
+    /* Container com o conteúdo principal: 
+    - Títulos com os nomes dos autores e a função do aplicativo
+    - Texto para exibir a mensagem de aviso
+    - Texto para exibir o valor do contador, com a cor definida pela função corValor()
+    - Botões para somar, subtrair e zerar o contador
+    */
 
-      <Text style={styles.titulo}>
+    <View style={styles.container}>
+      
+      <Text style={styles.titulo1}>
+        Cristian Marcheti e
+      </Text>
+
+      <Text style={styles.titulo1}> 
+        Maria Luiza Bonato - 3º D.S.
+      </Text>
+
+      <Text style={styles.titulo2}>
         Contador
       </Text>
 
       <Text style={styles.texto}>
-        {mensagem}
+        {mensagem} 
       </Text>
 
-      <Text style={[styles.texto, { color: corValor() }]}>
-        {valor}
+        
+      <Text style={[styles.texto, { color: corValor() }]}> 
+        {valor} 
       </Text>
 
       <View style={{flexDirection:'column', width: '30%', marginTop: 20 , marginBottom: 20}}>
-      <Button
+      <Button // Botão para somar 1 ao contador
         title="Somar 1"
         onPress={somarValor}
       />
       </View>
 
       <View style={{flexDirection:'column', width: '30%', marginBottom: 20}}>
-      <Button
+      <Button // Botão para subtrair 1 do contador
         title="Subtrair 1"
         onPress={subtrairValor}
       />
       </View>
 
       <View style={{flexDirection:'column', width: '30%', marginBottom: 20}}>
-      <Button
+      <Button // Botão para zerar o contador e a mensagem de aviso
         title="Zerar contador"
         onPress={zerarValor}
       />
@@ -82,7 +98,7 @@ export default function App() {
     
   )};
 
-
+// Estilos para a interface do aplicativo
 
 const styles = StyleSheet.create({
 
@@ -92,11 +108,18 @@ const styles = StyleSheet.create({
     alignItems:'center',  
     backgroundColor:'#daf0ff'
   },
+  
+  titulo1:{
+    fontSize:20,
+    fontWeight:'bold',
+    marginBottom:15
+  },
 
-  titulo:{
+  titulo2:{
     fontSize:26,
     fontWeight:'bold',
-    marginBottom:20
+    marginBottom:20,
+    marginTop: 20
   },
 
   texto:{
